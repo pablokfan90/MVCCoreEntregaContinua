19906a5d4b4c213dfbe5c0f95f9f3ef8af61d8a5
Mañanas <Maanas@GET-07>
2024-04-02 09:58:07 +0200

    Agregar archivos de proyecto.

---
+++ MVCCoreEntregaContinua/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using MVCCoreEntregaContinua.Models;
+using System.Diagnostics;
+
+namespace MVCCoreEntregaContinua.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["SALUDO"] = "Bienvenido a CI/CD GitHub";
+            Random random = new Random();
+            List<int> numeros = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                int num = random.Next(1, 20);
+                numeros.Add(num);
+            }
+            return View(numeros);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCCoreEntregaContinua/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCCoreEntregaContinua.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
